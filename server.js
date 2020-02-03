3523b9b6609489a2310152c822aa61ac21281867
Lindsey Brown <libersword@gmail.com>
2020-02-03 15:01:50 -0700

    Heroku Deploy

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
-// routes
+//Heroku
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongod://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
